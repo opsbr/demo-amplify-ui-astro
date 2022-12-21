1d569472d527d82195e8b0c4f5c42925f5aa8223
Ryosuke Iwanaga <riywo@opsbr.com>
2022-12-21 18:59:05 +0000

    Fix to run in SSR mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,26 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import node from "@astrojs/node";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  vite: {
+    ssr: {
+      noExternal: ["@aws-amplify/*", "@radix-ui/*"],
+    },
+    resolve: {
+      alias: {
+        "./runtimeConfig": "./runtimeConfig.browser",
+      },
+    },
+  },
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
